b6e49df72814a168b799d6c2e76b45a090474e5c
Yet Another Stupid Coder <117515380+WhyNeet@users.noreply.github.com>
2024-05-30 19:53:39 +0300

    feat(cli): validate user input

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::{env, process};
 
 use mee_core::*;
 
@@ -6,6 +6,12 @@ fn main() {
     let expr = env::args().nth(1).unwrap();
 
     let tokens = tokenizer::tokenize(&expr);
+
+    validator::validate(&tokens).unwrap_or_else(|err| {
+        eprintln!("\nan error occured: {err}\n");
+        process::exit(1);
+    });
+
     let ast = parser::parse(&tokens);
 
     let result = evaluator::evaluate(&ast);
