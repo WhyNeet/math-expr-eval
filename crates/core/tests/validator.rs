ddb5300d94c183a000f3c74540a3e49b5cc76992
Yet Another Stupid Coder <117515380+WhyNeet@users.noreply.github.com>
2024-05-30 18:39:31 +0300

    test(core): add validation tests

---
+++ crates/core/tests/validator.rs
@@ -0,0 +1,47 @@
+use mee_core::{
+    tokenizer,
+    validator::{self, error::ValidationError},
+};
+
+#[test]
+pub fn check_if_validator_works() {
+    let expr = "1 + 6 / 2";
+    let tokens = tokenizer::tokenize(expr);
+
+    assert!(validator::validate(&tokens).is_ok());
+
+    let expr = "1 + 6 / 2 -";
+    let tokens = tokenizer::tokenize(expr);
+
+    let validation = validator::validate(&tokens);
+
+    assert!(validation.is_err());
+    assert!(matches!(
+        validation.unwrap_err(),
+        ValidationError::SyntaxError
+    ));
+
+    let expr = "1 + 6 p 2 -";
+    let tokens = tokenizer::tokenize(expr);
+
+    let validation = validator::validate(&tokens);
+
+    assert!(validation.is_err());
+    assert!(matches!(
+        validation.unwrap_err(),
+        ValidationError::InvalidOperation('p')
+    ));
+
+    let expr = "(1 + 6) / ((2 - 5)";
+    let tokens = tokenizer::tokenize(expr);
+
+    let validation = validator::validate(&tokens);
+
+    println!("{validation:?}");
+
+    assert!(validation.is_err());
+    assert!(matches!(
+        validation.unwrap_err(),
+        ValidationError::InvalidParentheses
+    ));
+}
